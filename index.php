a7939eda49a05511d5c90ad193f20192f3890434
lukee <marciioluucas@gmail.com>
2017-04-04 20:23:53 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lukee
+ * Date: 4/4/17
+ * Time: 8:16 PM
+ */
---
+++ model/Usuario.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: lukee
+ * Date: 4/4/17
+ * Time: 8:13 PM
+ */
+class Usuario
+{
+
+}
