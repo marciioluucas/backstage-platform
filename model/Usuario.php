26651862639a7533e02a647ef26bdb92730e3c1d
JuanesAdriano <juaneshtk50@gmail.com>
2017-04-18 22:54:35 -0300

    Criando Classes a partir do banco

--- model/Usuario.php
+++ model/Usuario.php
@@ -8,5 +8,91 @@
  */
 class Usuario
 {
+private $pk_usuario;
+    private $nome;
+    private $matricula;
+    private $login;
+    private $senha;
+
+    /**
+     * @return mixed
+     */
+    public function getPkUsuario()
+    {
+        return $this->pk_usuario;
+    }
+
+    /**
+     * @param mixed $pk_usuario
+     */
+    public function setPkUsuario($pk_usuario)
+    {
+        $this->pk_usuario = $pk_usuario;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param mixed $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMatricula()
+    {
+        return $this->matricula;
+    }
+
+    /**
+     * @param mixed $matricula
+     */
+    public function setMatricula($matricula)
+    {
+        $this->matricula = $matricula;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * @param mixed $login
+     */
+    public function setLogin($login)
+    {
+        $this->login = $login;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSenha()
+    {
+        return $this->senha;
+    }
+
+    /**
+     * @param mixed $senha
+     */
+    public function setSenha($senha)
+    {
+        $this->senha = $senha;
+    }
+
 
 }
