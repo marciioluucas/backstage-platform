faba2c83f4547c4d09bf4ef06b7fbc01d1f70988
JuanesAdriano <juaneshtk50@gmail.com>
2017-04-25 19:56:30 -0300

    Modelando classes(voto, proposta)

---
+++ model/Voto.php
@@ -0,0 +1,22 @@
+<?php
+//TO MEXENO BY::JUCA
+/**
+ * Created by PhpStorm.
+ * User: ifgoiano
+ * Date: 25/04/2017
+ * Time: 19:48
+ */
+class Voto
+{
+
+    /**
+     * @var
+     */
+    private $pk_voto;
+    /**
+     * É a variável que irá guardar o balanço dos votos.
+     * @var
+     */
+    private $contagem;
+
+}
