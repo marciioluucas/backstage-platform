f54db9528cd8915b788ce44286df22c07b74ef48
JuanesAdriano <juaneshtk50@gmail.com>
2017-04-25 20:53:24 -0300

    Corrigindo classe Voto gets sets

--- model/Voto.php
+++ model/Voto.php
@@ -28,4 +28,54 @@ class Voto
      */
     private $fk_proposta;
 
+    /**
+     * @return mixed
+     */
+    public function getPkVoto()
+    {
+        return $this->pk_voto;
+    }
+
+    /**
+     * @param mixed $pk_voto
+     */
+    public function setPkVoto($pk_voto)
+    {
+        $this->pk_voto = $pk_voto;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFkUsuario()
+    {
+        return $this->fk_usuario;
+    }
+
+    /**
+     * @param mixed $fk_usuario
+     */
+    public function setFkUsuario($fk_usuario)
+    {
+        $this->fk_usuario = $fk_usuario;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFkProposta()
+    {
+        return $this->fk_proposta;
+    }
+
+    /**
+     * @param mixed $fk_proposta
+     */
+    public function setFkProposta($fk_proposta)
+    {
+        $this->fk_proposta = $fk_proposta;
+    }
+
+
+
 }
