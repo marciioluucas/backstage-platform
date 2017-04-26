efdf9cde330cbc8bb6915ba93641a9c9973f5af6
JuanesAdriano <juaneshtk50@gmail.com>
2017-04-25 22:29:16 -0300

    Atualizando banco

---
+++ backstage_platform.sql
@@ -0,0 +1,203 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 26-Abr-2017 às 03:26
+-- Versão do servidor: 10.1.21-MariaDB
+-- PHP Version: 7.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `backstage_platform`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `equipe`
+--
+
+CREATE TABLE `equipe` (
+  `pk_equipe` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `membro`
+--
+
+CREATE TABLE `membro` (
+  `pk_membro` int(11) NOT NULL,
+  `atuacao` varchar(255) NOT NULL,
+  `nivel` varchar(255) NOT NULL,
+  `fk_equipe` int(11) NOT NULL,
+  `funcao` varchar(50) NOT NULL,
+  `fk_usuario` int(20) NOT NULL,
+  `carga` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `projeto`
+--
+
+CREATE TABLE `projeto` (
+  `fk_equipe` int(11) NOT NULL,
+  `fk_proposta` int(11) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `proposta`
+--
+
+CREATE TABLE `proposta` (
+  `pk_proposta` int(11) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `descricao` text NOT NULL,
+  `fk_usuario` int(20) NOT NULL,
+  `data` datetime NOT NULL,
+  `contagem` int(11) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `pk_usuario` int(20) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `matricula` int(40) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `voto`
+--
+
+CREATE TABLE `voto` (
+  `pk_voto` int(11) NOT NULL,
+  `fk_usuario` int(20) NOT NULL,
+  `fk_proposta` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `equipe`
+--
+ALTER TABLE `equipe`
+  ADD PRIMARY KEY (`pk_equipe`);
+
+--
+-- Indexes for table `membro`
+--
+ALTER TABLE `membro`
+  ADD PRIMARY KEY (`pk_membro`,`fk_equipe`,`fk_usuario`),
+  ADD KEY `fk_programador_equipe1_idx` (`fk_equipe`),
+  ADD KEY `fk_membro_usuario1_idx` (`fk_usuario`);
+
+--
+-- Indexes for table `projeto`
+--
+ALTER TABLE `projeto`
+  ADD PRIMARY KEY (`fk_equipe`,`fk_proposta`),
+  ADD KEY `fk_equipe_has_proposta_proposta1_idx` (`fk_proposta`),
+  ADD KEY `fk_equipe_has_proposta_equipe1_idx` (`fk_equipe`);
+
+--
+-- Indexes for table `proposta`
+--
+ALTER TABLE `proposta`
+  ADD PRIMARY KEY (`pk_proposta`,`fk_usuario`),
+  ADD KEY `fk_Proposta_usuario_idx` (`fk_usuario`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`pk_usuario`);
+
+--
+-- Indexes for table `voto`
+--
+ALTER TABLE `voto`
+  ADD PRIMARY KEY (`pk_voto`,`fk_usuario`,`fk_proposta`),
+  ADD KEY `fk_voto_usuario1_idx` (`fk_usuario`),
+  ADD KEY `fk_voto_proposta1_idx` (`fk_proposta`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `equipe`
+--
+ALTER TABLE `equipe`
+  MODIFY `pk_equipe` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `membro`
+--
+ALTER TABLE `membro`
+  MODIFY `pk_membro` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `proposta`
+--
+ALTER TABLE `proposta`
+  MODIFY `pk_proposta` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `membro`
+--
+ALTER TABLE `membro`
+  ADD CONSTRAINT `fk_membro_usuario1` FOREIGN KEY (`fk_usuario`) REFERENCES `usuario` (`pk_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_programador_equipe1` FOREIGN KEY (`fk_equipe`) REFERENCES `equipe` (`pk_equipe`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `projeto`
+--
+ALTER TABLE `projeto`
+  ADD CONSTRAINT `fk_equipe_has_proposta_equipe1` FOREIGN KEY (`fk_equipe`) REFERENCES `equipe` (`pk_equipe`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_equipe_has_proposta_proposta1` FOREIGN KEY (`fk_proposta`) REFERENCES `proposta` (`pk_proposta`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `proposta`
+--
+ALTER TABLE `proposta`
+  ADD CONSTRAINT `fk_Proposta_usuario` FOREIGN KEY (`fk_usuario`) REFERENCES `usuario` (`pk_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `voto`
+--
+ALTER TABLE `voto`
+  ADD CONSTRAINT `fk_voto_proposta1` FOREIGN KEY (`fk_proposta`) REFERENCES `proposta` (`pk_proposta`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_voto_usuario1` FOREIGN KEY (`fk_usuario`) REFERENCES `usuario` (`pk_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
