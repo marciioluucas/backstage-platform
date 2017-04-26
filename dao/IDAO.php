306c1efd665c9f36794ea909135637cb86a4a161
lukee <marciioluucas@gmail.com>
2017-04-25 22:36:46 -0300

    Adicionando o IDAO e o USuarioDAO

---
+++ dao/IDAO.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lukee
+ * Date: 4/25/17
+ * Time: 10:33 PM
+ */
+
+namespace dao;
+
+
+interface IDAO
+{
+
+    function create();
+    function retreave();
+    function update();
+    function delete();
+
+}
---
+++ dao/UsuarioDAO.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lukee
+ * Date: 4/25/17
+ * Time: 10:35 PM
+ */
+
+namespace dao;
+
+
+class UsuarioDAO implements IDAO
+{
+
+    function create()
+    {
+        // TODO: Implement create() method.
+    }
+
+    function retreave()
+    {
+        // TODO: Implement retreave() method.
+    }
+
+    function update()
+    {
+        // TODO: Implement update() method.
+    }
+
+    function delete()
+    {
+        // TODO: Implement delete() method.
+    }
+}
