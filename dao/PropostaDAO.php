9b998d37b749de8c91f6ebae447106d3ca6b12ea
JuanesAdriano <juaneshtk50@gmail.com>
2017-05-04 22:25:16 -0300

    Criando o PropostaDAO

---
+++ dao/PropostaDAO.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ifgoiano
+ * Date: 04/05/2017
+ * Time: 20:45
+ */
+
+namespace dao;
+
+
+use phiber\Phiber;
+
+class PropostaDAO implements IDAO
+{
+
+    private $proposta;
+
+    function __construct($proposta)
+    {
+        $this->proposta = $proposta;
+    }
+
+    function create()
+    {
+       $phiber = new Phiber();
+       $criteria = $phiber->openPersist($this->proposta);
+       if ($criteria->create()){
+
+
+           return true;
+       }
+        xamppprint_r($criteria->show());
+           return false;
+
+
+    }
+
+    function update()
+    {
+        $phiber = new Phiber();
+        $criteria = $phiber->openPersist($this->proposta);
+        $restrictionID = $criteria->restrictions()->equals("pk_proposta", $this->proposta->getPkproposta());
+        $criteria->add($restrictionID);
+        if($criteria->update()){
+
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    function retreave()
+    {
+       $phiber = new Phiber();
+       $criteria = $phiber->openPersist($this->proposta);
+       $restrictions = [];
+
+       if($this->proposta->getTitulo() != null){
+           $restrictions[0] = $criteria->restrictions()->like("titulo", $this->getTitulo());
+       }
+
+        if($this->proposta->getFkusuario() != null){
+            $restrictions[2] = $criteria->restrictions()->equals("fk_usuario", $this->getFkusuario());
+        }
+
+        if($this->proposta->getPkproposta() !=null){
+            $restrictions[3] = $criteria->restrictions()->equals("pk_proposta", $this->getPkproposta());
+        }
+        if($this->proposta->getData() !=null){
+            $restrictions[4] = $criteria->restrictions()->like("data", $this->getData());
+        }
+
+    }
+
+    function delete()
+    {
+        $phiber = new Phiber();
+        $criteria = $phiber->openPersist($this->proposta);
+        $restrictionID = $criteria->restrictions()->equals("pk_proposta", $this->proposta->getPkproposta());
+        $criteria->add($restrictionID);
+        if ($criteria->delete()) {
+            return true;
+        } else {
+            return false;
+        }
+
+
+    }
+}
--- index.php
+++ index.php
@@ -7,22 +7,34 @@
  */
 require_once 'vendor/autoload.php';
 
-$u = new \model\Usuario();
+//$u = new \model\Usuario();
+//
+//$u->setPkUsuario(2);
+//$u->setNome("Lucas");
+//$u->setEmail("lucas@gmail.com");
+//$u->setMatricula("1234");
+//$u->setLogin("lukinhalokao");
+//$u->setSenha("1111");
+//
+//$dao = new \dao\UsuarioDAO($u);
+//$dao->update();
+//
+//$u2 = new \model\Usuario();
+////$u2->setNome("Ju");
+//$dao2 = new \dao\UsuarioDAO($u2);
+//
+//foreach ($dao2->retreave() as $usuario) {
+//    echo "ID: " . $usuario['pk_usuario'] . " Nome: " . $usuario['nome'] . " Email: " . $usuario['email'] . "\n";
+//}
 
-$u->setPkUsuario(2);
-$u->setNome("Lucas");
-$u->setEmail("lucas@gmail.com");
-$u->setMatricula("1234");
-$u->setLogin("lukinhalokao");
-$u->setSenha("1111");
+use model\Proposta;
+use dao\PropostaDAO;
+include "vendor/autoload.php";
+$proposta = new Proposta();
 
-$dao = new \dao\UsuarioDAO($u);
-$dao->update();
+$proposta->setTitulo("Arroiz");
+$proposta->setDescricao("e ce come");
 
-$u2 = new \model\Usuario();
-//$u2->setNome("Ju");
-$dao2 = new \dao\UsuarioDAO($u2);
+$DAO = new PropostaDAO($proposta);
 
-foreach ($dao2->retreave() as $usuario) {
-    echo "ID: " . $usuario['pk_usuario'] . " Nome: " . $usuario['nome'] . " Email: " . $usuario['email'] . "\n";
-}
+$DAO->create();
