d47c2ccdf6348f130a4b0c34c8441d98752e9a63
Sadrakss <outubro199625>
2017-05-09 10:01:32 -0300

    Mexendo na classe VotoController

--- controller/VotoController.php
+++ controller/VotoController.php
@@ -5,11 +5,36 @@
  * Date: 4/25/17
  * Time: 9:56 PM
  */
+                                        /**Sadrak Fazendo..*/
 
 namespace backstage\controller;
 
 
+use backstage\model\Voto;
+
 class VotoController
 {
+
+    private $voto;
+
+    /**
+     * VotoController constructor.
+     * @param $voto
+     */
+    public function __construct($voto)
+    {
+        $this->voto = new Voto();
+        if (isset($_POST['action'])&& $_POST['action'] == "cadastrar"){
+            $this->cadastrar();
+        }
+    }
+
+
+    public function cadastrar($values=null, $httpMethod = "GET"){
+        if ($httpMethod == "POST"){
+            $this->voto->setPkVoto($values['pk_voto']);
+        }
+    }
+
 
 }
