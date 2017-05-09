f08380ed834d8c715bf394a2c1bb4869fad14af2
Sadrakss <outubro199625>
2017-05-09 10:57:34 -0300

    Classe EquipeController, está pronta..

--- controller/EquipeController.php
+++ controller/EquipeController.php
@@ -14,6 +14,9 @@
 namespace backstage\controller;
 
 
+use backstage\model\Equipe;
+use backstage\util\Message;
+
 class EquipeController
 {
     private $equipe;
@@ -24,8 +27,36 @@ class EquipeController
      */
     public function __construct($equipe)
     {
-        $this->equipe = $equipe;
+        $this->equipe = new Equipe();
+        if (isset($_POST['action']) && $_POST['action'] == "cadastrar"){
+            $this->cadastrar();
+        }
     }
 
+    public function cadastrar($values = null, $httpMethod = "GET"){
+        if ($httpMethod == "POST"){
+            $this->equipe->setPkEquipe($values['pk_equipe']);
+            $this->equipe->setNome($values['nome']);
+                $r = new Message("Equipe Cadastrada com sucesso!", "sucesso", ["icone"=>"check"]);
+                echo $r->geraJsonMensagem();
+        }
+    }
 
+    public function alterar($values = null){
+        if ($values != null){
+            $this->equipe->setPkEquipe($values['pk_equipe']);
+            $this->equipe->setNome($values['nome']);
+                $r = new Message("Equipe alterada com sucesso!", "sucesso", ["icone" => "check" ]);
+                echo $r->geraJsonMensagem();
+        }
+    }
+
+    public function listar($values = null, $httpMethod = "GET"){
+        if ($httpMethod = "POST"){
+            $this->equipe(isset($values['pk_equipe']) ? $values['pk_equipe'] : null);
+            $this->equipe(isset($values['nome']) ? $values['nome'] : null);
+        }
+        echo json_encode($this->equipe->retreaveAll());
+    }
 }
+new EquipeController();
