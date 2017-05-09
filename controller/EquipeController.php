d65eaf706dd2d1bc072fb13737586c2fea871c7b
Sadrakss <outubro199625>
2017-05-09 10:31:26 -0300

    Mexendo na classe EquipeController

--- controller/EquipeController.php
+++ controller/EquipeController.php
@@ -3,13 +3,29 @@
  * Created by PhpStorm.
  * User: lukee
  * Date: 4/25/17
+ * Time: 9:54 PM                           Sadrak Fazendo..
+ */
+/**
+ * Created by PhpStorm.
+ * User: lukee
+ * Date: 4/25/17
  * Time: 9:54 PM
  */
-
 namespace backstage\controller;
 
 
 class EquipeController
 {
+    private $equipe;
+
+    /**
+     * EquipeController constructor.
+     * @param $equipe
+     */
+    public function __construct($equipe)
+    {
+        $this->equipe = $equipe;
+    }
+
 
 }
