d4fd62e34cd2273ff1f59733c6de37744285a628
Sadrakss <outubro199625>
2017-05-09 10:27:26 -0300

    Classe VotoController,está pronta..

--- controller/VotoController.php
+++ controller/VotoController.php
@@ -11,6 +11,7 @@ namespace backstage\controller;
 
 
 use backstage\model\Voto;
+use backstage\util\Message;
 
 class VotoController
 {
@@ -33,8 +34,33 @@ class VotoController
     public function cadastrar($values=null, $httpMethod = "GET"){
         if ($httpMethod == "POST"){
             $this->voto->setPkVoto($values['pk_voto']);
+            $this->voto->setFkUsuario($values['fk_usuario']);
+            $this->voto->setFkProposta($values['fk_proposta']);
+                $r= new Message("Voto cadastrado com sucesso!", "sucesso", ["icone"=>"check"]);
+                echo $r->geraJsonMensagem();
         }
     }
 
 
+    public function alterar ($values = null){
+        if($values != null){
+            $this->voto->setPkVoto($values['pk_voto']);
+            $this->voto->setFkUsuario($values['fk_usuario']);
+            $this->voto->setFkProposta($values['fk_proposta']);
+            if ($this->voto->atualizar()){
+                $r= new Message("Voto alterado com sucesso!","sucesso", ["icone" => "check"]);
+                echo $r->geraJsonMensagem();
+            }
+        }
+    }
+
+    public function listar($values = null, $httpMethod = "GET"){
+        if ($httpMethod = "POST"){
+            $this->voto->setPkVoto(isset($values['pk_voto']) ? $values['pk_voto'] : null);
+            $this->voto->setFkUsuario(isset($values['fk_usuario']) ? $values ['fk_usuario'] : null);
+            $this->voto->setFkProposta(isset($values['fk_proposta']) ? $values ['fk_proposta'] : null);
+        }
+        echo json_encode($this->voto->retreaveAll());
+    }
 }
+new VotoController();
